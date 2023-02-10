cb8b77527edce07bbd57750765a7a702718c40ae
joseluria <jos02-log18@hotmail.com>
2023-02-09 21:24:45 -0600

    refactor: :technologist: changed url validation

--- scrapper/validations.ts
+++ scrapper/validations.ts
@@ -1,14 +1,11 @@
 import { z } from 'zod'
 
-const siteRegex =
-  /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi
-
 export const jobSchema = z.object({
   title: z.string().min(1),
   company: z.string().min(1),
   location: z.string().min(1),
   image: z.string().min(1),
-  link: z.string().regex(siteRegex)
+  link: z.string().url()
 })
 
 export type Job = z.infer<typeof jobSchema>
