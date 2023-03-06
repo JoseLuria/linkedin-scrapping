4e095c024dc9c8fbbc6cdf5f4aafd6f08d44736b
JoseLuria <jos02-log18@hotmail.com>
2023-03-05 22:46:58 -0600

    refactor: :fire: changed fetch to axios

--- scrapper/utils.ts
+++ scrapper/utils.ts
@@ -1,10 +1,10 @@
 import { writeFile } from 'node:fs/promises'
 import path from 'node:path'
 import * as cheerio from 'cheerio'
+import axios from 'axios'
 
 export const scrape = async (URL: string): Promise<cheerio.CheerioAPI> => {
-  const res = await fetch(URL)
-  const html = await res.text()
+  const { data: html } = await axios.get(URL)
   return cheerio.load(html)
 }
 
